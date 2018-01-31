61bcae5964a7dbdd87ffacad143fed0974799c2a
vanishiment <1362137760@qq.com>
2018-01-31 19:57:51 +0800

    Settings

--- diaryapp/src/main/java/com/plant/diaryapp/activity/SettingsAct.java
+++ diaryapp/src/main/java/com/plant/diaryapp/activity/SettingsAct.java
@@ -2,8 +2,10 @@ package com.plant.diaryapp.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 
 import com.plant.diaryapp.R;
+import com.plant.diaryapp.fragment.SettingFrag;
 
 public class SettingsAct extends ToolbarAct {
 
@@ -11,6 +13,20 @@ public class SettingsAct extends ToolbarAct {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_settings);
+        initView();
+    }
+
+    private void initView(){
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setTitle(R.string.app_settings);
+        }
+        setDisplayShowTitleEnabled(true);
+        SettingFrag settingFrag = (SettingFrag) getSupportFragmentManager().findFragmentByTag("SettingFrag");
+        if (settingFrag == null){
+            settingFrag = new SettingFrag();
+            getSupportFragmentManager().beginTransaction().add(R.id.settings_container,settingFrag,"SettingFrag").commit();
+        }
     }
 
     @Override
